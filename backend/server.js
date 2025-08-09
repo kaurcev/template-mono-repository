e1c66200e91a3865a0a1e5e3242772a730085858
kaurcev <skaurcev2004@yandex.ru>
2025-08-09 17:31:19 +0600

    исправление косяков + паблик доступ

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 6769;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
-  console.log(`Сервер запущен на http://localhost:${port}`);
+  console.log(`Backend server running on http://localhost:${port}`);
 });
